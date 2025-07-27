176b2cfd0c7183a286e3ae30796e12984358af50
JoelChipoya <112080444+owellJoval@users.noreply.github.com>
2025-07-27 21:15:18 +0200

    first Pushed

---
+++ src/main/java/com/example/ytnafrica/YtnafricaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ytnafrica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtnafricaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YtnafricaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ytnafrica/YtnafricaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ytnafrica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtnafricaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
